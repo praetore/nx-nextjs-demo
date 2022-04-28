25c4df2673458c0232019967bf1f46f25f1d375c
darryl <darryl.amatsetam@finalist.nl>
2022-04-28 21:21:43 +0200

    move layout component dir

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout, {siteTitle} from '../components/layout'
+import Layout, {siteTitle} from '../app/layout/layout'
 import utilStyles from '../styles/utils.module.css'
 import {getPosts} from "@nx-nextjs-demo/posts";
 
